1f50aeda08117ce39176adbc2efde9b204330a1a
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-01 08:28:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,7 +53,7 @@ def get_average_age_for_season(data, season)
     end
   end
   age_array.each do |age|
-    integer_array << age.to_f
+    integer_array << age.to_i
   end
-  integer_array.reduce(:+) / integer_array.size
+  integer_array.reduce(:+).to_i / integer_array.size
 end
