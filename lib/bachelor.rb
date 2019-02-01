f6b74c3995bbd7b3d8a66ff3e6b7663b853d89a6
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-01 08:08:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -33,7 +33,12 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_num, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
