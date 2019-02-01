4c122ff697b849d45a38eb932cc7bffefb3f6bab
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-01 07:46:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,11 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_number, contestants|
+    if season_number == season
+      contestants
+    else
+      nil
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
